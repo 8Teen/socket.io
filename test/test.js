fde7d643c950d20deb95986f2dbaaab273a3a38a
jiangcheng.wxd <jiangcheng.wxd@taobao.com>
2015-10-30 11:55:53 +0800

    4 space

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ var http = require('http').Server;
 var io = require('..');
 var fs = require('fs');
 var join = require('path').join;
-var ioc = require('socket.io-client');
+	var ioc = require('socket.io-client');
 var request = require('supertest');
 var expect = require('expect.js');
 
@@ -21,38 +21,33 @@ function client(srv, nsp, opts) {
 	return ioc(url, opts);
 }
 
-it('should set the handshake BC object', function (done) {
-	var httpSrv = http();
-	var srv = io(httpSrv);
-
-	srv.on('connection', function (s) {
-		expect(s.handshake).to.not.be(undefined);
-
-		// Headers set and has some valid properties
-		expect(s.handshake.headers).to.be.an('object');
-		expect(s.handshake.headers['user-agent']).to.be('node-XMLHttpRequest');
-
-		// Time set and is valid looking string
-		expect(s.handshake.time).to.be.a('string');
-		expect(s.handshake.time.split(' ').length > 0); // Is "multipart" string representation
-
-		// Address, xdomain, secure, issued and url set
-		expect(s.handshake.address).to.contain('127.0.0.1');
-		expect(s.handshake.xdomain).to.be.a('boolean');
-		expect(s.handshake.secure).to.be.a('boolean');
-		expect(s.handshake.issued).to.be.a('number');
-		expect(s.handshake.url).to.be.a('string');
-
-		console.log(JSON.stringify(s.handshake));
-
-		// Query set and has some right properties
-		expect(s.handshake.query).to.be.an('object');
-		expect(s.handshake.query.EIO).to.not.be(undefined);
-		expect(s.handshake.query.transport).to.not.be(undefined);
-		expect(s.handshake.query.t).to.not.be(undefined);
-
-		done();
-	});
-
-	var socket = client(httpSrv);
+var a = io('localhost:6001/user',{
+	forceNew: true
 });
+
+a.on('connect', function (s) {
+	console.log("connected");
+});
+
+
+//it('should be able to close sio sending a port', function () {
+//	//for (var i = 0; i < 1001; i++) {
+//	//	ioc('ws://interactnode.daily.tmall.net:6001');
+//	//
+//	//	if (i == 1000) done();
+//	//}
+//
+//	//var a = ioc.connect('interactnode.daily.tmall.net:6001/user',{
+//	//	forceNew: true
+//	//});
+//
+//	var a = io('localhost:6001/user',{
+//		forceNew: true
+//	});
+//
+//	a.on('connect', function (s) {
+//		done();
+//	});
+//
+//	//if (i == 1000) done();
+//});
